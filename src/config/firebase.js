31291ecd794f9a04598368cd730866610707917e
acdmWeb <acdmdev@gmail.com>
2024-02-03 15:02:01 +0100

    variables de entorno local

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,12 @@
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbaqk1mRO2l9fBYxMblVnkUi5U_Vi8u7U",
-  authDomain: "point-of-sale-vuejs.firebaseapp.com",
-  projectId: "point-of-sale-vuejs",
-  storageBucket: "point-of-sale-vuejs.appspot.com",
-  messagingSenderId: "626052361717",
-  appId: "1:626052361717:web:3c8fe0fd29ad89e46344bc"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-export const firebaseApp = initializeApp(firebaseConfig)
+export const firebaseApp = initializeApp(firebaseConfig);
